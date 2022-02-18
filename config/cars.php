58a9ca21ee99876b23c593502780452369927e39
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2022-02-18 11:56:31 +0100

    scaffolding, model, seeder

---
+++ config/cars.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+    [
+        "brand" => "Ferrari",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque, nisl sed pulvinar condimentum, quam magna dignissim arcu, vitae hendrerit dolor magna sit amet leo. Praesent pretium nunc velit, pulvinar fermentum massa volutpat quis. Nam ut odio semper, tempus magna et, tristique justo. Duis a elementum lacus. Ut pretium tortor id enim interdum, sed tempor ligula malesuada. Morbi nisl sapien, euismod ut feugiat a, gravida sed justo. Ut ut dui non risus volutpat maximus. Sed turpis diam, rhoncus sit amet posuere rutrum, laoreet eget tellus. In nulla sapien, suscipit ut ipsum in, malesuada dictum diam. Integer porta ex elit, placerat elementum tellus eleifend ut. Sed aliquam nulla sit amet massa feugiat, sit amet lobortis sapien faucibus. Nulla tempus eros vel dolor aliquam, at cursus lorem scelerisque.",
+        "src" => "https://www.repstatic.it/content/nazionale/img/2021/08/26/222057314-8e93a706-5564-43a7-bd10-450d5e558861.jpg",
+        "price" => "2500000",
+        "model" => "Corporate",
+        "cc" => "300cc"
+    ],
+    [
+        "brand" => "Audi",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque, nisl sed pulvinar condimentum, quam magna dignissim arcu, vitae hendrerit dolor magna sit amet leo. Praesent pretium nunc velit, pulvinar fermentum massa volutpat quis. Nam ut odio semper, tempus magna et, tristique justo. Duis a elementum lacus. Ut pretium tortor id enim interdum, sed tempor ligula malesuada. Morbi nisl sapien, euismod ut feugiat a, gravida sed justo. Ut ut dui non risus volutpat maximus. Sed turpis diam, rhoncus sit amet posuere rutrum, laoreet eget tellus. In nulla sapien, suscipit ut ipsum in, malesuada dictum diam. Integer porta ex elit, placerat elementum tellus eleifend ut. Sed aliquam nulla sit amet massa feugiat, sit amet lobortis sapien faucibus. Nulla tempus eros vel dolor aliquam, at cursus lorem scelerisque.",
+        "src" => "https://immagini.alvolante.it/sites/default/files/styles/anteprima_lunghezza_640/public/news_anteprima/2016/10/audi-r8-e-tron_21.jpg",
+        "price" => "1000000",
+        "model" => "R8 e-tron",
+        "cc" => "350cc"
+    ],
+    [
+        "brand" => "Bugatti",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque, nisl sed pulvinar condimentum, quam magna dignissim arcu, vitae hendrerit dolor magna sit amet leo. Praesent pretium nunc velit, pulvinar fermentum massa volutpat quis. Nam ut odio semper, tempus magna et, tristique justo. Duis a elementum lacus. Ut pretium tortor id enim interdum, sed tempor ligula malesuada. Morbi nisl sapien, euismod ut feugiat a, gravida sed justo. Ut ut dui non risus volutpat maximus. Sed turpis diam, rhoncus sit amet posuere rutrum, laoreet eget tellus. In nulla sapien, suscipit ut ipsum in, malesuada dictum diam. Integer porta ex elit, placerat elementum tellus eleifend ut. Sed aliquam nulla sit amet massa feugiat, sit amet lobortis sapien faucibus. Nulla tempus eros vel dolor aliquam, at cursus lorem scelerisque.",
+        "src" => "https://i2.wp.com/asphalt9.info/wp-content/uploads/2021/09/Bugatti-Veyron-16.4-Grand-Sport-Vitesse.jpg?fit=1062%2C588&ssl=1",
+        "price" => "3000000",
+        "model" => "Veyron",
+        "cc" => "500cc"
+    ],
+    [
+        "brand" => "Lamborghini",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque, nisl sed pulvinar condimentum, quam magna dignissim arcu, vitae hendrerit dolor magna sit amet leo. Praesent pretium nunc velit, pulvinar fermentum massa volutpat quis. Nam ut odio semper, tempus magna et, tristique justo. Duis a elementum lacus. Ut pretium tortor id enim interdum, sed tempor ligula malesuada. Morbi nisl sapien, euismod ut feugiat a, gravida sed justo. Ut ut dui non risus volutpat maximus. Sed turpis diam, rhoncus sit amet posuere rutrum, laoreet eget tellus. In nulla sapien, suscipit ut ipsum in, malesuada dictum diam. Integer porta ex elit, placerat elementum tellus eleifend ut. Sed aliquam nulla sit amet massa feugiat, sit amet lobortis sapien faucibus. Nulla tempus eros vel dolor aliquam, at cursus lorem scelerisque.",
+        "src" => "https://www.autotecnica.org/wp-content/uploads/2019/01/529099.jpg",
+        "price" => "1350000",
+        "model" => "Uragan",
+        "cc" => "1000cc"
+    ],
+    [
+        "brand" => "Pagani",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque, nisl sed pulvinar condimentum, quam magna dignissim arcu, vitae hendrerit dolor magna sit amet leo. Praesent pretium nunc velit, pulvinar fermentum massa volutpat quis. Nam ut odio semper, tempus magna et, tristique justo. Duis a elementum lacus. Ut pretium tortor id enim interdum, sed tempor ligula malesuada. Morbi nisl sapien, euismod ut feugiat a, gravida sed justo. Ut ut dui non risus volutpat maximus. Sed turpis diam, rhoncus sit amet posuere rutrum, laoreet eget tellus. In nulla sapien, suscipit ut ipsum in, malesuada dictum diam. Integer porta ex elit, placerat elementum tellus eleifend ut. Sed aliquam nulla sit amet massa feugiat, sit amet lobortis sapien faucibus. Nulla tempus eros vel dolor aliquam, at cursus lorem scelerisque.",
+        "src" => "https://images.everyeye.it/img-notizie/pagani-lavoro-hypercar-elettrica-conferma-fondatore-v3-346524-1280x720.jpg",
+        "price" => "1240000",
+        "model" => "Zonda Riviera",
+        "cc" => "650cc"
+    ],
+    [
+        "brand" => "Tesla",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque, nisl sed pulvinar condimentum, quam magna dignissim arcu, vitae hendrerit dolor magna sit amet leo. Praesent pretium nunc velit, pulvinar fermentum massa volutpat quis. Nam ut odio semper, tempus magna et, tristique justo. Duis a elementum lacus. Ut pretium tortor id enim interdum, sed tempor ligula malesuada. Morbi nisl sapien, euismod ut feugiat a, gravida sed justo. Ut ut dui non risus volutpat maximus. Sed turpis diam, rhoncus sit amet posuere rutrum, laoreet eget tellus. In nulla sapien, suscipit ut ipsum in, malesuada dictum diam. Integer porta ex elit, placerat elementum tellus eleifend ut. Sed aliquam nulla sit amet massa feugiat, sit amet lobortis sapien faucibus. Nulla tempus eros vel dolor aliquam, at cursus lorem scelerisque.",
+        "src" => "https://www.tesla.com/sites/default/files/images/roadster/roadster-social.jpg",
+        "price" => "800000",
+        "model" => "Roadster",
+        "cc" => "400cc"
+    ],
+    [
+        "brand" => "Aston Martin",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque, nisl sed pulvinar condimentum, quam magna dignissim arcu, vitae hendrerit dolor magna sit amet leo. Praesent pretium nunc velit, pulvinar fermentum massa volutpat quis. Nam ut odio semper, tempus magna et, tristique justo. Duis a elementum lacus. Ut pretium tortor id enim interdum, sed tempor ligula malesuada. Morbi nisl sapien, euismod ut feugiat a, gravida sed justo. Ut ut dui non risus volutpat maximus. Sed turpis diam, rhoncus sit amet posuere rutrum, laoreet eget tellus. In nulla sapien, suscipit ut ipsum in, malesuada dictum diam. Integer porta ex elit, placerat elementum tellus eleifend ut. Sed aliquam nulla sit amet massa feugiat, sit amet lobortis sapien faucibus. Nulla tempus eros vel dolor aliquam, at cursus lorem scelerisque.",
+        "src" => "https://cdn.motor1.com/images/mgl/02em2/s1/aston-martin-valhalla.jpg",
+        "price" => "4000000",
+        "model" => "Valhalla",
+        "cc" => "800cc"
+    ],
+    [
+        "brand" => "Porsche",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque, nisl sed pulvinar condimentum, quam magna dignissim arcu, vitae hendrerit dolor magna sit amet leo. Praesent pretium nunc velit, pulvinar fermentum massa volutpat quis. Nam ut odio semper, tempus magna et, tristique justo. Duis a elementum lacus. Ut pretium tortor id enim interdum, sed tempor ligula malesuada. Morbi nisl sapien, euismod ut feugiat a, gravida sed justo. Ut ut dui non risus volutpat maximus. Sed turpis diam, rhoncus sit amet posuere rutrum, laoreet eget tellus. In nulla sapien, suscipit ut ipsum in, malesuada dictum diam. Integer porta ex elit, placerat elementum tellus eleifend ut. Sed aliquam nulla sit amet massa feugiat, sit amet lobortis sapien faucibus. Nulla tempus eros vel dolor aliquam, at cursus lorem scelerisque.",
+        "src" => "https://c8.alamy.com/compit/pktbkj/15-settembre-2018-2019-porsche-911-gt3-rs-viene-fornito-in-dotazione-con-un-4-0-litri-boxer-6-hp-520-in-lucertola-verde-pktbkj.jpg",
+        "price" => "400000",
+        "model" => "Porsche GT3 RS",
+        "cc" => "250cc"
+    ],
+    [
+        "brand" => "Porsche",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque, nisl sed pulvinar condimentum, quam magna dignissim arcu, vitae hendrerit dolor magna sit amet leo. Praesent pretium nunc velit, pulvinar fermentum massa volutpat quis. Nam ut odio semper, tempus magna et, tristique justo. Duis a elementum lacus. Ut pretium tortor id enim interdum, sed tempor ligula malesuada. Morbi nisl sapien, euismod ut feugiat a, gravida sed justo. Ut ut dui non risus volutpat maximus. Sed turpis diam, rhoncus sit amet posuere rutrum, laoreet eget tellus. In nulla sapien, suscipit ut ipsum in, malesuada dictum diam. Integer porta ex elit, placerat elementum tellus eleifend ut. Sed aliquam nulla sit amet massa feugiat, sit amet lobortis sapien faucibus. Nulla tempus eros vel dolor aliquam, at cursus lorem scelerisque.",
+        "src" => "https://www.autoblog.it/app/uploads/2021/07/porsche-macan-gts-2022-1600-09.jpeg",
+        "price" => "250000",
+        "model" => "Macan",
+        "cc" => "350cc"
+    ],
+    [
+        "brand" => "Dodge",
+        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque, nisl sed pulvinar condimentum, quam magna dignissim arcu, vitae hendrerit dolor magna sit amet leo. Praesent pretium nunc velit, pulvinar fermentum massa volutpat quis. Nam ut odio semper, tempus magna et, tristique justo. Duis a elementum lacus. Ut pretium tortor id enim interdum, sed tempor ligula malesuada. Morbi nisl sapien, euismod ut feugiat a, gravida sed justo. Ut ut dui non risus volutpat maximus. Sed turpis diam, rhoncus sit amet posuere rutrum, laoreet eget tellus. In nulla sapien, suscipit ut ipsum in, malesuada dictum diam. Integer porta ex elit, placerat elementum tellus eleifend ut. Sed aliquam nulla sit amet massa feugiat, sit amet lobortis sapien faucibus. Nulla tempus eros vel dolor aliquam, at cursus lorem scelerisque.",
+        "src" => "https://www.autoscout24.it/assets/auto/images/model/dodge/dodge-viper/dodge-viper-l-01.jpg",
+        "price" => "200000",
+        "model" => "Viper",
+        "cc" => "500cc"
+    ],
+];
