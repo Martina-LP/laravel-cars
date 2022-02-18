4d5f613674d0f520472242a995ff8b4df27cbae7
Martina-LP <madlp8@gmail.com>
2022-02-18 12:22:16 +0100

    Home, nav, route

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Cars</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Lista Auto</a>
+                    <a class="nav-link" aria-current="page" href="#">Crea Nuove Auto</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </header>
+
+    <main>
+        @yield('main_content')
+    </main>
+
+</body>
+</html>
