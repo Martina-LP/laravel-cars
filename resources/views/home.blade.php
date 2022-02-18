683272eac4a2c9e8db02cd2dd066a2dce227302f
Martina-LP <madlp8@gmail.com>
2022-02-18 13:16:59 +0100

    Front end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,5 @@
 @extends('layouts.app')
-<<<<<<< HEAD
 
 @section('main_content')
     <h1>Ecco la home</h1>
 @endsection
-=======
-@section('main_content')
-    <h1>
-        Ciao sono la HOme
-    </h1>
-
-@endsection
->>>>>>> 694aabcee30c6168bdae4c24e6a4d1a2e2a70a8f
